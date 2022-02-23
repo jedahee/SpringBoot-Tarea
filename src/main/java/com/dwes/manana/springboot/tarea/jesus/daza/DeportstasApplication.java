e07e4d4d5fd3c97981826b2ac6569283c7e8ea03
Jesus <jedahee02@gmail.com>
2022-02-23 12:29:50 +0100

    Inicializando

---
+++ src/main/java/com/dwes/manana/springboot/tarea/jesus/daza/DeportstasApplication.java
@@ -0,0 +1,13 @@
+package com.dwes.manana.springboot.tarea.jesus.daza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeportstasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeportstasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dwes/manana/springboot/tarea/jesus/daza/DeportstasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwes.manana.springboot.tarea.jesus.daza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeportstasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
